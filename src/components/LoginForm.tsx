b7ca677500da330dcc00b998d191df0bd6edc54d
fedjes <salvados78@mail.ru>
2023-05-09 18:06:45 +0300

    login

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,20 +1,36 @@
 import React, { useState } from 'react';
 import react, { FormEventHandler } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+import UserPage from './UserPage';
+import { render } from '@testing-library/react';
+import { Route, redirect } from "react-router-dom";
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
+  const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
+  const history = useNavigate();
 
   const handleSubmit: FormEventHandler = (event) => { 
     event.preventDefault(); 
+    axios.post("https://ancient-smoke-8603.fly.dev/login", {login, password})
+    .then((response) => {
+     let localStorageId = response.data.id;
+     localStorage.setItem('userId', localStorageId);
+      console.log(response.data);
+      history('/user');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Username:
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} />
       </label>
       <label>
         Password:
